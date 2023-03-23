ed80aa40b76d5116fbbcb4df19ae727e0e570173
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-22 20:41:03 -0600

    Interface IBinarySearchTree V1.0

--- IBinarySearchTree.java
+++ IBinarySearchTree.java
@@ -1,3 +1,36 @@
-public class IBinarySearchTree {
-    
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+
+ /**
+  * @author Pablo Orellana 
+  * Interface Binary Search Tree
+  */
+
+public interface IBinarySearchTree <K,V> {
+    /**
+     * Metodo para insertar valores
+     * @param id Llave 
+     * @param value Valor
+     */
+
+    void insert(K id, V value);
+
+    /**
+     * Metodo para eliminar valores
+     * @param id Llave del valor para eliminar
+     * @return valor eliminado
+     */
+
+    V delete(K id);
+
+    /**
+     * Metodo para buscar valores
+     * @param id Llave a buscar
+     * @return valor que se buscó
+     */
+
+    V find(K id); 
 }
