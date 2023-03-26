a09670029e7429464e28e37c2da9bc659b524b71
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-25 19:23:18 -0600

    Interface IBinarySearchTree V2.0

--- IBinarySearchTree.java
+++ IBinarySearchTree.java
@@ -32,5 +32,41 @@ public interface IBinarySearchTree <K,V> {
      * @return valor que se buscó
      */
 
-    V find(K id); 
+    V find(K id);
+    
+    /**
+	   * Metodo que permite dar la cantidad de objetos que hay en el arbol
+	   * @return tamaño del arbol
+	   */
+	  int count();
+	
+	  /**
+	   * 
+	   * @return True si esta vacio y false en caso contrario
+	   */
+	  boolean isEmpty();
+	
+	  /**
+	   * Metodo que regresa un listado de los elementos
+	   * @return listado de elementos
+	   */
+	  ArrayList<V> getElements();
+	
+	  /**
+	   * Regresa valores en orden
+	   * @param traversal
+	   */
+	  void inOrder(ITreeTraversal<V> traversal);
+	
+	  /**
+	   * Regresa valores pre orden
+	   * @param traversal
+	   */
+	  void preOrder(ITreeTraversal<V> traversal);
+	
+	  /**
+	   * Regresa valores post orden
+	   * @param traversal
+	   */
+	  void postOrder(ITreeTraversal<V> traversal);
 }
