08f80b4f44ba9dd10b658f79eda478903d4190d8
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-25 19:33:59 -0600

    StringComparator V1.0

--- StringComparator.java
+++ StringComparator.java
@@ -1,3 +1,15 @@
-public class StringComparator {
-    
+/**
+ * 
+ */
+import java.util.Comparator;
+/**
+ * Clase String Comparator, encargada de comparar strings
+ * @author Pablo Orellana
+ */
+
+public class StringComparator implements Comparator<String>{
+    @Override
+	public int compare(String o1, String o2) {
+		return o1.compareTo(o2);
+	}
 }
