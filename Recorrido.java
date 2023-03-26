fe168121fcd7011197e3824a31fcfda31c49e1d3
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-25 19:45:47 -0600

    Clase Recorrido V1.0

--- Recorrido.java
+++ Recorrido.java
@@ -1,3 +1,17 @@
-public class Recorrido {
-    
+/**
+ * 
+ */
+import java.util.ArrayList;
+/**
+ * Clase Recorrido
+ * @author Pablo Orellana
+ */
+
+public class Recorrido<V> implements ITreeTraversal<V> {
+    public ArrayList<V> miLista = new ArrayList<V>();
+	
+	@Override
+	public void Walk(V value) {
+		miLista.add(value);
+	}
 }
